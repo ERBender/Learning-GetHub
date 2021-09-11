23a9771f21fddb900a856ba028caa36001e633d7
Erwin Bender <bender@erbender.com>
2021-09-11 14:00:31 -0500

    Initial Commit

---
+++ Learning GetHub again/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Learning GetHub again
+//
+//  Created by Erwin Bender on 9/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Learning GetHub again/Learning_GetHub_againApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_GetHub_againApp.swift
+//  Learning GetHub again
+//
+//  Created by Erwin Bender on 9/11/21.
+//
+
+import SwiftUI
+
+@main
+struct Learning_GetHub_againApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
